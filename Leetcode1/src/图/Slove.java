77c2487b2e8db0049c6347a009375489bd0d34fa
Zhang-Bocheng <854528079@qq.com>
2024-07-23 18:08:51 +0800

    图-洪水填充2

---
+++ Leetcode1/src/图/Slove.java
@@ -0,0 +1,57 @@
+package 图;
+
+/**
+ * ClassName: Slove
+ * Package: 图
+ * Description: https://leetcode.cn/problems/surrounded-regions/
+ *
+ * @Author zbc
+ * @Create 2024/7/23 下午4:35
+ * @Version 1.0
+ */
+public class Slove {
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][m - 1] == 'O') {
+                dfs(board, i, m - 1);
+            }
+        }
+
+        for (int j = 1; j < m - 1; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+            if (board[n - 1][j] == 'O') {
+                dfs(board, n - 1, j);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = 'A';
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+
+}
